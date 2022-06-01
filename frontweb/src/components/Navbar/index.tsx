7f9f09519101e045ee4ecf7e5881cd8fd5ccefe9
Kameloth <kameloth2004@hotmail.com>
2022-06-01 20:28:25 -0300

    Navbar styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,14 +1,25 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-md navbar-light bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav offset-md-2 main-menu">
+        
+        <button className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#carrostop-navbar"
+        aria-controls="carrostop-navbar"
+        aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse" id="carrostop-navbar">
+          <ul className="navbar-nav offset-md-10 main-menu">
             <li>
               <a href="link" className="nav-home">HOME</a>
             </li>
