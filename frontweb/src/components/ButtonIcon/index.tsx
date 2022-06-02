ac191aa82862cf5cdb41b834a786b18c24641ddf
Kameloth <kameloth2004@hotmail.com>
2022-06-02 15:22:49 -0300

    Mobile 320px default

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+
+    return (
+        <button className="btn btn-secondary btn-icon">
+            <h2>VER CATÁLOGO</h2>
+        </button>
+    );
+}
+
+export default ButtonIcon;
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { ReactComponent as HeaderImage } from "assets/images/car-header.svg";
+import Navbar from "components/Navbar";
+import ButtonIcon from "components/ButtonIcon";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="header-container">
+        <div className="home-content-container">
+          <h1>O carro perfeito para você</h1>
+          <p>
+            Conheça nossos carros e dê mais um passo na realização do seu sonho
+          </p>
+        </div>
+        <div className="home-image-container">
+          <HeaderImage />
+        </div>
+      </div>
+      
+      <div className="button-container">
+      <ButtonIcon />
+        <p>Comece agora a Navegar</p>
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
